7a975ee9c51b8f09f0d2557f29ce5f3889805a3c
Abdulla-hamideh <Abdullahhamideh@outlook.com>
2021-05-22 01:45:41 +0300

    Update cleaning the data.py

--- cleaning the data.py
+++ cleaning the data.py
@@ -1,3 +1,4 @@
+# importing the necessary libraries
 import pandas as pd
 import re
 
@@ -10,27 +11,21 @@ print(data.head(10).to_string())
 # deleting the unnamed coloumn
 del data["Unnamed: 0"]
 
-# descriptive statistics
-print(data.info())
-print(data.describe().to_string())
-
-
 #cleaning the text
 tweet = data["tweet"]
 clean_tweet = []
 remove = ["#","&",'"',";","!",":","/","http","~"]
 
-
+# replacing the words
 for i in tweet:
     line = i[:i.find("/")]
     for g in remove:
         line = re.sub(g," ",line)
-    line = re.sub("(@[A-Za-z0-9_]+)","rt",line)
     line = re.sub("RT","",line)
     clean_tweet.append(line)
 
+# adding it to the dataframe
 data["clean_tweet"] = clean_tweet
-print(data.head(100).to_string())
 
 # convert into a new csv
 data.to_csv("clean_tweets.csv", index=False)
